6812108058275392c84218470c1c206af91848de
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-13 09:32:12 +0300

    Initial commit

---
+++ src/main/java/com/colak/springtutorial/SpringApplication.java
@@ -0,0 +1,16 @@
+package com.colak.springtutorial;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@Slf4j
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+    }
+
+}
